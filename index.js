85f2dfa711698efbf224636d35d70f19bc07d998
Gangabhavani622 <115251057+Gangabhavani622@users.noreply.github.com>
2022-11-02 00:29:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-let month = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+
 function add(days) {
   var addDays = require("date-fns/addDays");
   let date1 = new Date("22 Aug 2020");
   let result = addDays(date1, days);
-  return `${result.getDate()} ${
-    month[result.getMonth()]
-  } ${result.getFullYear()}`;
+  return `${result.getDate()}-${
+    month[result.getMonth()+1]
+  }-${result.getFullYear()}`;
 }
 console.log(add(5));
 module.exports = add;
